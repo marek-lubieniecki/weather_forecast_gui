b546ac46590dbd0ec8373724aeca3d0dedec2076
ml <marek.lubieniecki@spaceforest.pl>
2021-07-23 23:19:23 +0200

    Project creation 2

---
+++ WeatherForecastGui/myapp.py
@@ -0,0 +1,27 @@
+# Filename: signals_slots.py
+
+"""Signals and slots example."""
+
+import sys
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My app")
+
+        button = QPushButton("Press Me!")
+        self.setFixedSize((QSize(400,300)))
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
